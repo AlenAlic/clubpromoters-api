89ed5231dc42c9f7cff01698993699158e677035
Alen Alic <aalic89@gmail.com>
2020-06-03 19:52:04 +0200

    fix path split on linux, typo

--- models/file/model.py
+++ models/file/model.py
@@ -12,22 +12,22 @@ class File(db.Model, TrackModifications):
     name = db.Column(db.String(256), nullable=False)
     logo = db.Column(db.Boolean, nullable=False, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey(f"{TABLE_USERS}.user_id"))
-    user = db.relationship('User', back_populates='files')
+    user = db.relationship("User", back_populates="files")
 
     def __repr__(self):
         return f"{self.file_id}: {self.name}"
 
     @property
     def directory(self):
-        return self.path.replace('\\', '/').rsplit("\\", 1)[0]
+        return self.path.replace("\\", "/").rsplit("/", 1)[0]
 
     @property
     def filename(self):
-        return self.path.replace('\\', '/').rsplit("\\", 1)[1]
+        return self.path.replace("\\", "/").rsplit("/", 1)[1]
 
     def url(self):
         relative_url = "{static}{file}"\
-            .format(static=current_app.static_url_path, file=self.path.split("static")[1].replace('\\', '/'))
+            .format(static=current_app.static_url_path, file=self.path.split("static")[1].replace("\\", "/"))
         url = f"{request.scheme}://{request.host}{relative_url}"
         return urllib.parse.quote(url, safe="/:")
 

3785ebf43336fe842c05897143692f32b475d641
Alen Alic <aalic89@gmail.com>
2020-06-03 15:50:47 +0200

    add attachments to email sending

--- mailing/send.py
+++ mailing/send.py
@@ -10,11 +10,15 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 
-def send_email(subject, recipients, text_body, html_body, cc=None, bcc=None):
+def send_email(subject, recipients, text_body, html_body, cc=None, bcc=None, attachments=None):
     if current_app.config['DEBUG'] and config().test_email:
         recipients = [config().test_email]
     msg = Message(subject, recipients=recipients, cc=cc, bcc=bcc)
     msg.body = text_body
     msg.html = html_body
+    if attachments:
+        for name, attachment in attachments.items():
+            with current_app.open_resource(attachment) as file:
+                msg.attach(filename=name, data=file.read())
     # noinspection PyProtectedMember
     Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()

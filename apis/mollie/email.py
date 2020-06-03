5748a10e3b2483aee9a881762e97cb5a7e531442
Alen Alic <aalic89@gmail.com>
2020-06-03 14:02:07 +0200

    move purchase emails

--- apis/mollie/email.py
+++ apis/mollie/email.py
@@ -5,5 +5,5 @@ from mailing import send_email
 def send_purchased_tickets(purchase):
     send_email(f"Tickets for {purchase.party}",
                recipients=[purchase.email], bcc=[],
-               text_body=render_template('email/purchased_tickets.txt', purchase=purchase),
-               html_body=render_template('email/purchased_tickets.html', purchase=purchase))
+               text_body=render_template("email/purchase/purchased_tickets.txt", purchase=purchase),
+               html_body=render_template("email/purchase/purchased_tickets.html", purchase=purchase))

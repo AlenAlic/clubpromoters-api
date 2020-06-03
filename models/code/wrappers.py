f085b16e682331ac57e9d04483d4978d18059ca4
Alen Alic <aalic89@gmail.com>
2020-06-03 17:11:13 +0200

    fix code required wrapper

--- models/code/wrappers.py
+++ models/code/wrappers.py
@@ -6,8 +6,8 @@ from models import get_code_from_request
 
 def code_required(f):
     @wraps(f)
-    def decorated_function(**kwargs):
+    def decorated_function(*args, **kwargs):
         if not get_code_from_request(request):
             return "", HTTPStatus.PRECONDITION_REQUIRED.value
-        return f(**kwargs)
+        return f(*args, **kwargs)
     return decorated_function

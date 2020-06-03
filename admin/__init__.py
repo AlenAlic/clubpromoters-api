3ccfa222a7c25b15a61be2205edfa45b12d89ca6
Alen Alic <aalic89@gmail.com>
2020-06-03 17:30:11 +0200

    admin login screen

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,6 +1,6 @@
 from flask_admin import Admin
 from .views import MyAdminIndexView, BaseView, UserView
-from models import User, Location, Party, Purchase, Ticket, Refund, Code, File
+from models import User, Location, Party, Purchase, Ticket, Refund, Code, File, PartyFile
 
 
 admin = Admin(name="clubpromoters.net", template_mode="bootstrap3", index_view=MyAdminIndexView())
@@ -16,3 +16,4 @@ def init_app(app, db):
     admin.add_view(BaseView(Refund, db.session))
     admin.add_view(BaseView(Code, db.session))
     admin.add_view(BaseView(File, db.session))
+    admin.add_view(BaseView(PartyFile, db.session))
